f26f11fde38e6aff13a36b8feef40c773c980792
Charlie Li <129420947+ChengoBoi@users.noreply.github.com>
2024-11-02 19:24:26 +0100

    Memory_main.py

--- Memory_main.py
+++ Memory_main.py
@@ -1,1 +1,5 @@
 #Ska börja med den snart
+#Ska ta inspiration från oline och med hjälp av ChatGpt
+#Här är projektspecifikationen från tidigare studenter som har valt att göra 193 Memory 
+https://www.studocu.com/sv/document/kungliga-tekniska-hogskolan/programmeringsteknik/puppgift-projspec-projspec-till-memory/79929741
+https://www.studocu.com/sv/document/kungliga-tekniska-hogskolan/programmeringsteknik/memory-p-uppgift-projektspec/93259017
